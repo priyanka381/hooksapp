2f68b6999d070d8302a8a89ea5889190d5ce8df9
priyanka381 <84623060+priyanka381@users.noreply.github.com>
2021-11-22 16:48:19 +0530

    fetch the data and use space

--- src/App.js
+++ src/App.js
@@ -1,50 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
-
-import React ,{useReducer}from 'react';
+import React, { useReducer } from 'react';
 import CounterTwo from './components/reducerhook/CounterTwo';
 import CounterThree from './components/reducerhook/CounterThree';
 import CompA from './components/contextNreducer/CompA';
 import CompB from './components/contextNreducer/CompB';
 import CompC from './components/contextNreducer/CompC';
-import CompD from './components/contextNreducer/CompD';
-import CompF from './components/contextNreducer/CompF';
+import DataFectching from './components/usereducercases/Datafetching';
+import DataFetchingTwo from './components/usereducercases/DataFetchingTwo';
+
 
 export const CountContext = React.createContext()
 const initialState = 0
-
-const reducer = (state,action) => {
-    switch(action){
-        case 'increment': return state+1
-        case 'decrement': return state-1
-        case 'reset': return initialState
-        default: return state
-    }
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment': return state + 1
+    case 'decrement': return state - 1
+    case 'reset': return initialState
+    default: return state
+  }
 }
 
 function App() {
-  const [count,dispatch] = useReducer(reducer,initialState)
   return (
-    <CountContext.Provider 
-              value = {  
-                        {countState: count, countDispatch: dispatch} 
-                       }>
-    <div className="App">
-  
-     
-    Count in app.js is {count}
-      <CompA/>
-      <CompB/>
-      <CompC/>
-      <CompD/>
-      <CompF/>
+<div className="App">
+<DataFectching/>
+<DataFetchingTwo/>
     </div>
-    </CountContext.Provider>
-  );
-}
+    );
+  }
 
-      
-    
+
+
 
 export default App;
