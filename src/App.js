81b932bb2eb89164e5586cd7e61ded0ba43c6833
priyanka381 <84623060+priyanka381@users.noreply.github.com>
2021-11-22 12:12:09 +0530

    contexthook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
+
+import CounterOne from './components/reducerhook/CounterOne';
+import CounterTwo from './components/reducerhook/CounterTwo';
+import ComponentA from './components/contexthook/ComponentA';
+import ComponentB from './components/contexthook/ComponentB';
+import ComponentC from './components/contexthook/ComponentC';
+import ComponentE from './components/contexthook/ComponentE';
+
+export const UserContext = React.createContext()     //exporting a context
+export const BatchContext = React.createContext()
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <CounterOne/>
+     <CounterTwo/>
+     <ComponentA/>
+     <ComponentB/>
+     <ComponentC/>
+     <ComponentE/>
+
+      
     </div>
   );
 }
